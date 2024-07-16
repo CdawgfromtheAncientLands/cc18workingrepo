efe290d86769d02cce6087ff0d65a8792b28a213
CdawgfromtheAncientLands <cmrbusiness123456@gmail.com>
2024-07-16 18:29:55 -0400

    Created list of movies to use

---
+++ media-toolbar/src/components/Movies.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+//Set list of movies to 
+const defaultMovieRating = 1;
+//List of movies
+const moviesData = [
+  {title: 'Good Will Hunting', rating: defaultMovieRating, image: 'url1'},
+  {title: 'The Good, the Bad and the Ugly', rating: defaultMovieRating, image: 'url2'},
+  {title: 'A Few Good Men', rating: defaultMovieRating, image: 'url3'},
+  {title: '12 Angry Men', rating: defaultMovieRating, image: 'url4'},
+  {title: 'The Good Dinosaur', rating: defaultMovieRating, image: 'url5'},
+  {title: 'Good Morning, Vietnam', rating: defaultMovieRating, image: 'url6'},
+  {title: 'American Dad', rating: defaultMovieRating, image: 'url7'},
+];
